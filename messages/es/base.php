17d297e4399a269160b7a3a7b6edfb601fbfdab0
HumHub Translations <info@humhub.com>
2021-05-18 20:26:22 +0200

    Enh: Updated Translations (translate.humhub.org)

--- messages/es/base.php
+++ messages/es/base.php
@@ -1,12 +1,11 @@
 <?php
-
-return [
-    'Bookmarked' => 'Marcado como favorito',
-    'Bookmarks' => 'Favoritos',
-    'Remove from bookmarks' => 'Quitar de favoritos',
-    'Save as bookmark' => 'Guardar como favorito',
-    'Content successfully bookmarked. You can access it directly from your profile.' => '',
-    'Content successfully removed from bookmarks.' => '',
-    'Invalid content id given!' => '',
-    'You cannot bookmark this content!' => '',
-];
+return array (
+  'Bookmarked' => 'Marcado como favorito',
+  'Bookmarks' => 'Favoritos',
+  'Content successfully bookmarked. You can access it directly from your profile.' => 'Contenido incluido con éxito en favoritos. Puedes acceder a él directamente desde tu perfil.',
+  'Content successfully removed from bookmarks.' => 'Contenido eliminado con éxito de los favoritos.',
+  'Invalid content id given!' => '¡Se ha proporcionado un identificador de contenido inválido!',
+  'Remove from bookmarks' => 'Quitar de favoritos',
+  'Save as bookmark' => 'Guardar como favorito',
+  'You cannot bookmark this content!' => '¡No puedes incluir este contenido en los favoritos!',
+);
--- messages/pl/base.php
+++ messages/pl/base.php
@@ -1,12 +1,11 @@
 <?php
-
-return [
-    'Bookmarked' => '',
-    'Bookmarks' => '',
-    'Content successfully bookmarked. You can access it directly from your profile.' => '',
-    'Content successfully removed from bookmarks.' => '',
-    'Invalid content id given!' => '',
-    'Remove from bookmarks' => '',
-    'Save as bookmark' => '',
-    'You cannot bookmark this content!' => '',
-];
+return array (
+  'Bookmarked' => 'Dodano zakładkę',
+  'Bookmarks' => 'Zakładki',
+  'Content successfully bookmarked. You can access it directly from your profile.' => 'Treść dodana do zakładek. Możesz się do niej dostać bezpośrednio ze swojego profilu.',
+  'Content successfully removed from bookmarks.' => 'Treść poprawnie usunięta z zakładek.',
+  'Invalid content id given!' => 'Podano niepoprawne id treści!',
+  'Remove from bookmarks' => 'Usuń z zakładek',
+  'Save as bookmark' => 'Zapisz jako zakładka',
+  'You cannot bookmark this content!' => 'Nie możesz dodać tej treści do zakładek!',
+);
