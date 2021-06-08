5b55543e54368a48f2468cc4f465d37f890204ee
HumHub Translations <info@humhub.com>
2021-06-08 21:54:36 +0200

    Enh: Updated Translations (translate.humhub.org)

--- messages/de/base.php
+++ messages/de/base.php
@@ -2,10 +2,10 @@
 return array (
   'Bookmarked' => 'Mit Lesezeichen',
   'Bookmarks' => 'Lesezeichen',
-  'Content successfully bookmarked. You can access it directly from your profile.' => '',
-  'Content successfully removed from bookmarks.' => '',
+  'Content successfully bookmarked. You can access it directly from your profile.' => 'Inhalt erfolgreich mit einem Lesezeichen versehen. Du kannst nun jederzeit direkt aus deinem Profil darauf zugreifen!',
+  'Content successfully removed from bookmarks.' => 'Lesezeichen erfolgreich entfernt.',
   'Invalid content id given!' => 'Ungültige Inhalts-ID angegeben!',
   'Remove from bookmarks' => 'Lesezeichen entfernen',
   'Save as bookmark' => 'Lesezeichen setzen',
-  'You cannot bookmark this content!' => '',
+  'You cannot bookmark this content!' => 'Dieser Inhalt kann nicht mit einem Lesezeichen versehen werden!',
 );
