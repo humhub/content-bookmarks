ec4bc16c3f7fe742b70623543f9f1bbc7141b14e
HumHub Translations <info@humhub.com>
2021-06-10 20:13:34 +0200

    Enh: Updated Translations (translate.humhub.org)

--- messages/th/base.php
+++ messages/th/base.php
@@ -1,12 +1,11 @@
 <?php
-
-return [
-    'Bookmarked' => '',
-    'Bookmarks' => '',
-    'Content successfully bookmarked. You can access it directly from your profile.' => '',
-    'Content successfully removed from bookmarks.' => '',
-    'Invalid content id given!' => '',
-    'Remove from bookmarks' => '',
-    'Save as bookmark' => '',
-    'You cannot bookmark this content!' => '',
-];
+return array (
+  'Bookmarked' => 'บุ๊คมาร์ค',
+  'Bookmarks' => 'ที่คั่นหนังสือ',
+  'Content successfully bookmarked. You can access it directly from your profile.' => 'บุ๊กมาร์กเนื้อหาเรียบร้อยแล้ว คุณสามารถเข้าถึงได้โดยตรงจากโปรไฟล์ของคุณ',
+  'Content successfully removed from bookmarks.' => 'นำเนื้อหาออกจากบุ๊กมาร์กเรียบร้อยแล้ว',
+  'Invalid content id given!' => 'ระบุ ID เนื้อหาไม่ถูกต้อง!',
+  'Remove from bookmarks' => 'นำออกจากบุ๊กมาร์ก',
+  'Save as bookmark' => 'บันทึกเป็นบุ๊คมาร์ค',
+  'You cannot bookmark this content!' => 'คุณไม่สามารถบุ๊กมาร์กเนื้อหานี้ได้!',
+);
