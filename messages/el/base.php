5ab4299c9a60bd9fbe2a8c8f2aaee5141ea7735a
HumHub Translations <info@humhub.com>
2021-06-11 10:23:33 +0200

    Enh: Updated Translations (translate.humhub.org)

--- messages/el/base.php
+++ messages/el/base.php
@@ -1,11 +1,11 @@
 <?php
 return array (
-  'Bookmarked' => '',
-  'Bookmarks' => '',
-  'Content successfully bookmarked. You can access it directly from your profile.' => '',
-  'Content successfully removed from bookmarks.' => '',
+  'Bookmarked' => 'Προστέθηκε στους Σελιδοδείκτες',
+  'Bookmarks' => 'Σελιδοδείκτες',
+  'Content successfully bookmarked. You can access it directly from your profile.' => 'Το περιεχόμενο προστέθηκε επιτυχώς στους σελιδοδείκτες. Μπορείτε να το δείτε απευθείας από το προφίλ σας.',
+  'Content successfully removed from bookmarks.' => 'Το περιεχόμενο αφαιρέθηκε επιτυχώς από τους σελιδοδείκτες',
   'Invalid content id given!' => 'Μη έγκυρο αναγνωριστικό ID περιεχομένου!',
-  'Remove from bookmarks' => '',
-  'Save as bookmark' => '',
-  'You cannot bookmark this content!' => '',
+  'Remove from bookmarks' => 'Διαγραφή από τους σελιδοδείκτες',
+  'Save as bookmark' => 'Αποθήκευση ως σελιδοδείκτης',
+  'You cannot bookmark this content!' => 'Δεν μπορείτε να δημιουργήσετε σελιδοδείκτη από αυτό το περιεχόμενο!',
 );
