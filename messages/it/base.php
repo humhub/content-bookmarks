f40fc0416f40f30eda8ff52710fc0e5aa6c32e7f
HumHub Translations <info@humhub.com>
2021-06-23 11:29:23 +0200

    Enh: Updated Translations (translate.humhub.org)

--- messages/it/base.php
+++ messages/it/base.php
@@ -2,10 +2,10 @@
 return array (
   'Bookmarked' => 'Segnalibro aggiunto',
   'Bookmarks' => 'Segnalibri',
-  'Content successfully bookmarked. You can access it directly from your profile.' => '',
-  'Content successfully removed from bookmarks.' => '',
+  'Content successfully bookmarked. You can access it directly from your profile.' => 'Contenuto aggiunto ai preferiti. Puoi accedervi direttamente dal tuo profilo.',
+  'Content successfully removed from bookmarks.' => 'Contenuto rimosso con successo dai segnalibri.',
   'Invalid content id given!' => 'Il Content id fornito non è valido!',
   'Remove from bookmarks' => 'Rimuovi dai segnalibri',
   'Save as bookmark' => 'Aggiungi segnalibro',
-  'You cannot bookmark this content!' => '',
+  'You cannot bookmark this content!' => 'Non puoi aggiungere questo contenuto ai preferiti!',
 );
