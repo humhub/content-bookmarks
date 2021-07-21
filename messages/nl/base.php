30d265610484627e518e04155a37780f67e660df
HumHub Translations <info@humhub.com>
2021-07-21 18:19:24 +0200

    Enh: Updated Translations (translate.humhub.org)

--- messages/nl/base.php
+++ messages/nl/base.php
@@ -1,13 +1,12 @@
 <?php
-
-return [
-    'Bookmarked' => 'Bladwijzer gemaakt',
-    'Bookmarks' => 'Bladwijzer',
-    'Content successfully bookmarked. You can access it directly from your profile.' => 'Bladwijzer gemaakt van deze informatie. Je hebt er rechtstreeks toegang toe vanuit je profiel.',
-    'Content successfully removed from bookmarks.' => 'Informatie succesvol verwijderd uit bladwijzers.',
-    'Invalid content id given!' => 'Ongeldige informatie-ID opgegeven!',
-    'Remove from bookmarks' => 'Verwijderen uit bladwijzers',
-    'Save as bookmark' => 'Opslaan als bladwijzer',
-    'You cannot bookmark this content!' => 'U kunt van deze informatie geen bladwijzer maken!',
-    'You didn\'t save any content yet!' => '',
-];
+return array (
+  'Bookmarked' => 'Bladwijzer gemaakt',
+  'Bookmarks' => 'Bladwijzer',
+  'Content successfully bookmarked. You can access it directly from your profile.' => 'Bladwijzer gemaakt van deze informatie. Je hebt er rechtstreeks toegang toe vanuit je profiel.',
+  'Content successfully removed from bookmarks.' => 'Informatie succesvol verwijderd uit bladwijzers.',
+  'Invalid content id given!' => 'Ongeldige informatie-ID opgegeven!',
+  'Remove from bookmarks' => 'Verwijderen uit bladwijzers',
+  'Save as bookmark' => 'Opslaan als bladwijzer',
+  'You cannot bookmark this content!' => 'U kunt van deze informatie geen bladwijzer maken!',
+  'You didn\'t save any content yet!' => 'U hebt nog geen informatie opgeslagen!',
+);
