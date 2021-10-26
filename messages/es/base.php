74a4ae5fc8fc22efeb53a6c42d29938931b25575
HumHub Translations <info@humhub.com>
2021-10-26 14:31:01 +0200

    Enh: Updated Translations (translate.humhub.org)

--- messages/es/base.php
+++ messages/es/base.php
@@ -1,13 +1,12 @@
 <?php
-
-return [
-    'Bookmarked' => 'Marcado como favorito',
-    'Bookmarks' => 'Favoritos',
-    'Content successfully bookmarked. You can access it directly from your profile.' => 'Contenido incluido con éxito en favoritos. Puedes acceder a él directamente desde tu perfil.',
-    'Content successfully removed from bookmarks.' => 'Contenido eliminado con éxito de los favoritos.',
-    'Invalid content id given!' => '¡Se ha proporcionado un identificador de contenido inválido!',
-    'Remove from bookmarks' => 'Quitar de favoritos',
-    'Save as bookmark' => 'Guardar como favorito',
-    'You cannot bookmark this content!' => '¡No puedes incluir este contenido en los favoritos!',
-    'You didn\'t save any content yet!' => '',
-];
+return array (
+  'Bookmarked' => 'Marcado como favorito',
+  'Bookmarks' => 'Favoritos',
+  'Content successfully bookmarked. You can access it directly from your profile.' => 'Contenido incluido con éxito en favoritos. Puedes acceder a él directamente desde tu perfil.',
+  'Content successfully removed from bookmarks.' => 'Contenido eliminado con éxito de los favoritos.',
+  'Invalid content id given!' => '¡Se ha proporcionado un identificador de contenido inválido!',
+  'Remove from bookmarks' => 'Quitar de favoritos',
+  'Save as bookmark' => 'Guardar como favorito',
+  'You cannot bookmark this content!' => '¡No puedes incluir este contenido en los favoritos!',
+  'You didn\'t save any content yet!' => '¡Aún no has guardado ningún contenido!',
+);
