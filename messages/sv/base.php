7ebe5df38bd3e03abd3ad39224123b8def1703ea
HumHub Translations <info@humhub.com>
2021-12-07 18:54:09 +0100

    Enh: Updated Translations (translate.humhub.org)

--- messages/sv/base.php
+++ messages/sv/base.php
@@ -1,13 +1,12 @@
 <?php
-
-return [
-    'Bookmarked' => 'Bokmärkt',
-    'Bookmarks' => 'Bokmärken',
-    'Invalid content id given!' => 'Felaktigt innehålls id angivet!',
-    'Remove from bookmarks' => 'Ta bort bokmärke',
-    'Save as bookmark' => 'Spara som bokmärke',
-    'Content successfully bookmarked. You can access it directly from your profile.' => '',
-    'Content successfully removed from bookmarks.' => '',
-    'You cannot bookmark this content!' => '',
-    'You didn\'t save any content yet!' => '',
-];
+return array (
+  'Bookmarked' => 'Bokmärkt',
+  'Bookmarks' => 'Bokmärken',
+  'Content successfully bookmarked. You can access it directly from your profile.' => 'Innehållet har bokmärkts. Du kan komma åt det direkt från din profil.',
+  'Content successfully removed from bookmarks.' => 'Innehållet har tagits bort från bokmärken.',
+  'Invalid content id given!' => 'Felaktigt innehålls id angivet!',
+  'Remove from bookmarks' => 'Ta bort bokmärke',
+  'Save as bookmark' => 'Spara som bokmärke',
+  'You cannot bookmark this content!' => 'Du kan inte bokmärka det här innehållet!',
+  'You didn\'t save any content yet!' => 'Du har inte sparat något innehåll än!',
+);
