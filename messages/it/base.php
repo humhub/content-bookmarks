d76b4b3caf29b7e95ef354aeb57356942cfa7365
HumHub Translations <info@humhub.com>
2021-12-23 11:57:30 +0100

    Enh: Updated Translations (translate.humhub.org)

--- messages/it/base.php
+++ messages/it/base.php
@@ -1,13 +1,12 @@
 <?php
-
-return [
-    'Bookmarked' => 'Segnalibro aggiunto',
-    'Bookmarks' => 'Segnalibri',
-    'Content successfully bookmarked. You can access it directly from your profile.' => 'Contenuto aggiunto ai preferiti. Puoi accedervi direttamente dal tuo profilo.',
-    'Content successfully removed from bookmarks.' => 'Contenuto rimosso con successo dai segnalibri.',
-    'Invalid content id given!' => 'Il Content id fornito non è valido!',
-    'Remove from bookmarks' => 'Rimuovi dai segnalibri',
-    'Save as bookmark' => 'Aggiungi segnalibro',
-    'You cannot bookmark this content!' => 'Non puoi aggiungere questo contenuto ai preferiti!',
-    'You didn\'t save any content yet!' => '',
-];
+return array (
+  'Bookmarked' => 'Segnalibro aggiunto',
+  'Bookmarks' => 'Segnalibri',
+  'Content successfully bookmarked. You can access it directly from your profile.' => 'Contenuto aggiunto ai preferiti. Puoi accedervi direttamente dal tuo profilo.',
+  'Content successfully removed from bookmarks.' => 'Contenuto rimosso con successo dai segnalibri.',
+  'Invalid content id given!' => 'Il Content id fornito non è valido!',
+  'Remove from bookmarks' => 'Rimuovi dai segnalibri',
+  'Save as bookmark' => 'Aggiungi segnalibro',
+  'You cannot bookmark this content!' => 'Non puoi aggiungere questo contenuto ai preferiti!',
+  'You didn\'t save any content yet!' => 'Non hai ancora salvato alcun contenuto!',
+);
