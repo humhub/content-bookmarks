9cbd01fd5e526d9a671a010deb9c235a3f6ed45b
HumHub Translations <hello@humhub.com>
2022-09-06 16:32:33 +0000

    Enh: Updated Translations (translate.humhub.org)

--- messages/pt/base.php
+++ messages/pt/base.php
@@ -1,13 +1,12 @@
 <?php
-
-return [
-    'Invalid content id given!' => 'Id de conteúdo fornecido inválido!',
-    'Bookmarked' => '',
-    'Bookmarks' => '',
-    'Content successfully bookmarked. You can access it directly from your profile.' => '',
-    'Content successfully removed from bookmarks.' => '',
-    'Remove from bookmarks' => '',
-    'Save as bookmark' => '',
-    'You cannot bookmark this content!' => '',
-    'You didn\'t save any content yet!' => '',
-];
+return array (
+  'Bookmarked' => 'Adicionado aos marcadores',
+  'Bookmarks' => 'Marcadores',
+  'Content successfully bookmarked. You can access it directly from your profile.' => 'Conteúdo adicionado aos marcadores com sucesso. Pode acedê-lo diretamente a partir do seu perfil.',
+  'Content successfully removed from bookmarks.' => 'Conteúdo removido dos marcadores com sucesso.',
+  'Invalid content id given!' => 'Id de conteúdo fornecido inválido!',
+  'Remove from bookmarks' => 'Remover dos marcadores',
+  'Save as bookmark' => 'Guardar aos marcadores',
+  'You cannot bookmark this content!' => 'Não pode adicionar este conteúdo aos marcadores!',
+  'You didn\'t save any content yet!' => 'Ainda não guardou qualquer conteúdo!',
+);
