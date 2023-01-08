66e945381d835f18d5b976198f58e12289ae57f5
HumHub Translations <hello@humhub.com>
2023-01-08 15:46:09 +0000

    Enh: Updated Translations (translate.humhub.org)

--- messages/hu/base.php
+++ messages/hu/base.php
@@ -1,13 +1,12 @@
 <?php
-
-return [
-    'Invalid content id given!' => 'A megadott tartalomazonosító érvénytelen!',
-    'Bookmarked' => '',
-    'Bookmarks' => '',
-    'Content successfully bookmarked. You can access it directly from your profile.' => '',
-    'Content successfully removed from bookmarks.' => '',
-    'Remove from bookmarks' => '',
-    'Save as bookmark' => '',
-    'You cannot bookmark this content!' => '',
-    'You didn\'t save any content yet!' => '',
-];
+return array (
+  'Bookmarked' => 'Elmentve',
+  'Bookmarks' => 'Könyvjelzők',
+  'Content successfully bookmarked. You can access it directly from your profile.' => 'Sikeresen elmentve a könyvjelzőkhöz. A profilodon keresztül érheted el.',
+  'Content successfully removed from bookmarks.' => 'Eltávolítva a könyvjelzőkből.',
+  'Invalid content id given!' => 'A megadott tartalomazonosító érvénytelen!',
+  'Remove from bookmarks' => 'Eltávolítás',
+  'Save as bookmark' => 'Mentés',
+  'You cannot bookmark this content!' => 'Nem lehet elmenteni ezt a bejegyzést!',
+  'You didn\'t save any content yet!' => 'Még nem mentettél el semmit!',
+);
