5c6bba4ed6806a556b6bb4bb3da5a56d6c9d95a6
HumHub Translations <hello@humhub.com>
2023-02-28 07:37:50 +0000

    Enh: Updated Translations (translate.humhub.org)

--- messages/zh-TW/base.php
+++ messages/zh-TW/base.php
@@ -1,13 +1,12 @@
 <?php
-
-return [
-    'Bookmarked' => '',
-    'Bookmarks' => '',
-    'Content successfully bookmarked. You can access it directly from your profile.' => '',
-    'Content successfully removed from bookmarks.' => '',
-    'Invalid content id given!' => '',
-    'Remove from bookmarks' => '',
-    'Save as bookmark' => '',
-    'You cannot bookmark this content!' => '',
-    'You didn\'t save any content yet!' => '',
-];
+return array (
+  'Bookmarked' => '已收藏',
+  'Bookmarks' => '收藏的內容',
+  'Content successfully bookmarked. You can access it directly from your profile.' => '內容成功被收藏。您可以在您的個人檔案頁面直接存取。',
+  'Content successfully removed from bookmarks.' => '內容成功從收藏中移除。',
+  'Invalid content id given!' => '錯誤的內容ID!',
+  'Remove from bookmarks' => '從收藏中移除',
+  'Save as bookmark' => '儲存到我的收藏',
+  'You cannot bookmark this content!' => '您無法收藏這個內容!',
+  'You didn\'t save any content yet!' => '您沒有任何收藏的內容!',
+);
