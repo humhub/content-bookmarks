1cc32a8248e35c022e814c2596c46013e6886b90
HumHub Translations <hello@humhub.com>
2023-05-03 16:59:34 +0000

    Enh: Updated Translations (translate.humhub.org)

--- messages/ja/base.php
+++ messages/ja/base.php
@@ -4,9 +4,9 @@ return array (
   'Bookmarks' => 'ブックマーク',
   'Content successfully bookmarked. You can access it directly from your profile.' => 'コンテンツが正常にブックマークされました。プロフィールから直接アクセスできます。',
   'Content successfully removed from bookmarks.' => 'ブックマークからコンテンツが正常に削除されました。',
-  'Invalid content id given!' => '無効なコンテンツIDが指定されました。',
+  'Invalid content id given!' => '無効なコンテンツIDが指定されました！',
   'Remove from bookmarks' => 'ブックマークから削除',
   'Save as bookmark' => 'ブックマークとして保存',
-  'You cannot bookmark this content!' => 'このコンテンツをブックマークする事は出来ません',
-  'You didn\'t save any content yet!' => 'まだコンテンツを保存していません',
+  'You cannot bookmark this content!' => 'このコンテンツをブックマークする事は出来ません！',
+  'You didn\'t save any content yet!' => 'まだコンテンツを保存していません！',
 );
