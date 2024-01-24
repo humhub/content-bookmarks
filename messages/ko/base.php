9bf6d9e740314fc12cf825d708fd29d7e43f32c0
HumHub Translations <hello@humhub.com>
2024-01-24 19:37:48 +0000

    Enh: Updated Translations (translate.humhub.org)

--- messages/ko/base.php
+++ messages/ko/base.php
@@ -1,13 +1,12 @@
 <?php
-
-return [
-    'Bookmarked' => '',
-    'Bookmarks' => '',
-    'Content successfully bookmarked. You can access it directly from your profile.' => '',
-    'Content successfully removed from bookmarks.' => '',
-    'Invalid content id given!' => '',
-    'Remove from bookmarks' => '',
-    'Save as bookmark' => '',
-    'You cannot bookmark this content!' => '',
-    'You didn\'t save any content yet!' => '',
-];
+return array (
+  'Bookmarked' => '',
+  'Bookmarks' => '',
+  'Content successfully bookmarked. You can access it directly from your profile.' => '',
+  'Content successfully removed from bookmarks.' => '',
+  'Invalid content id given!' => '잘못된 콘텐츠 ID가 입력되었습니다!',
+  'Remove from bookmarks' => '',
+  'Save as bookmark' => '',
+  'You cannot bookmark this content!' => '',
+  'You didn\'t save any content yet!' => '',
+);
