e62d23c692e86757c26f7d556c74e0a666fde040
HumHub Translations <hello@humhub.com>
2024-01-24 19:31:14 +0000

    Enh: Updated Translations (translate.humhub.org)

--- messages/cs/base.php
+++ messages/cs/base.php
@@ -1,13 +1,12 @@
 <?php
-
-return [
-    'Invalid content id given!' => 'Vloženo chybé ID obsahu',
-    'Bookmarked' => '',
-    'Bookmarks' => '',
-    'Content successfully bookmarked. You can access it directly from your profile.' => '',
-    'Content successfully removed from bookmarks.' => '',
-    'Remove from bookmarks' => '',
-    'Save as bookmark' => '',
-    'You cannot bookmark this content!' => '',
-    'You didn\'t save any content yet!' => '',
-];
+return array (
+  'Bookmarked' => 'Záložky',
+  'Bookmarks' => 'Záložky',
+  'Content successfully bookmarked. You can access it directly from your profile.' => 'Obsah byl úspěšně přidán do záložek. Můžete k němu přistupovat přímo ze svého profilu.',
+  'Content successfully removed from bookmarks.' => 'Obsah byl úspěšně odstraněn ze záložek.',
+  'Invalid content id given!' => 'Vloženo chybé ID obsahu',
+  'Remove from bookmarks' => 'Odebrat ze záložek',
+  'Save as bookmark' => 'Přidat do záložek',
+  'You cannot bookmark this content!' => 'Tento obsah nelze přidat do záložek!',
+  'You didn\'t save any content yet!' => 'Zatím jste neuložili žádný obsah!',
+);
