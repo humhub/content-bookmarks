560c9d064c4f909b5ed29e5a5a86601870fcce4c
HumHub Translations <hello@humhub.com>
2024-04-20 18:01:17 +0000

    Enh: Updated Translations (translate.humhub.org)

--- messages/pl/base.php
+++ messages/pl/base.php
@@ -1,13 +1,12 @@
 <?php
-
-return [
-    'Bookmarked' => 'Dodano zakładkę',
-    'Bookmarks' => 'Zakładki',
-    'Content successfully bookmarked. You can access it directly from your profile.' => 'Treść dodana do zakładek. Możesz się do niej dostać bezpośrednio ze swojego profilu.',
-    'Content successfully removed from bookmarks.' => 'Treść poprawnie usunięta z zakładek.',
-    'Invalid content id given!' => 'Podano niepoprawne id treści!',
-    'Remove from bookmarks' => 'Usuń z zakładek',
-    'Save as bookmark' => 'Zapisz jako zakładka',
-    'You cannot bookmark this content!' => 'Nie możesz dodać tej treści do zakładek!',
-    'You didn\'t save any content yet!' => '',
-];
+return array (
+  'Bookmarked' => 'Dodano zakładkę',
+  'Bookmarks' => 'Zakładki',
+  'Content successfully bookmarked. You can access it directly from your profile.' => 'Treść dodana do zakładek. Możesz się do niej dostać bezpośrednio ze swojego profilu.',
+  'Content successfully removed from bookmarks.' => 'Treść poprawnie usunięta z zakładek.',
+  'Invalid content id given!' => 'Podano niepoprawne id treści!',
+  'Remove from bookmarks' => 'Usuń z zakładek',
+  'Save as bookmark' => 'Zapisz jako zakładka',
+  'You cannot bookmark this content!' => 'Nie możesz dodać tej treści do zakładek!',
+  'You didn\'t save any content yet!' => 'Nie zapisano żadnej zawartości!',
+);
